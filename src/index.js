fd18cf541526cefccbc8e75c928b5dcc37f909e0
pranalyadav <yadav.pranal94@gmail.com>
2025-01-28 16:09:46 +0530

    redux tutorial

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import store from "./store.js"
+import { Provider } from 'react-redux';
+
+// store.dispatch({ type: 'account/deposit', payload: 250 });
+// console.log(store.getState());
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+
